435cc5edf80b065120dd21937e14232923fb5244
John N. Norman <normanj777@gmail.com>
2022-07-06 11:41:36 -0400

    design components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="temperature-component">
+        <div className="temperature-display">
+          <p>0</p>
+        </div>
+        <div className="temperature-controls">
+          <button>-</button>
+          <button>+</button>
+        </div>
+      </div>
     </div>
   );
 }
